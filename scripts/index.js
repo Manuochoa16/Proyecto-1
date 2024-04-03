56c6bddc2b958812768bc1e70028758f9201b10c
manuochoa16 <nicomanu2316@gmail.com>
2024-04-03 12:40:09 -0300

    Repositorio 03042024

--- scripts/index.js
+++ scripts/index.js
@@ -8,18 +8,28 @@ class Activity {
 }
 
 class Repository {
-    constructor(){
-        this.activities = {};
+    constructor(){        this.activities = [];
         this.id = 0;
     }
     getAllActivities(){
         return this.activities;
     }
-    createActivity(id, title, description, imgUrl) {
-        const activity = new Activity(id, title, description, imgUrl);
+    createActivity(title, description, imgUrl) {
+        this.id++;
+        const activity = new Activity(this.id, title, description, imgUrl);
         this.activities.push(activity);
     }
+
     deleteActivity(id) {
         this.activities = this.activities.filter(activity => activity.id !== id);
+        return this.activities;
     }
 }
+
+const repository = new Repository()
+
+console.log(repository);
+
+repository.createActivity('Estudiar', 'Crear habitos para aplicar lo aprendido', 'https://t1.uc.ltmcdn.com/es/posts/6/7/0/trucos_para_estudiar_mas_rapido_53076_orig.jpg')
+
+console.log(repository.getAllActivities());
